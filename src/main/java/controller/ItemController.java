bae297c4f1f15f1b94fe4f55812533d87079f302
sourav <souravsengupta20192019@gmail.com>
2021-09-15 20:00:20 +0530

    Latest-1

---
+++ src/main/java/controller/ItemController.java
@@ -0,0 +1,23 @@
+package controller;
+
+public class ItemController {
+
+    public void createItem(){
+        System.out.println("Create Item Controller");
+    }
+     public void updateItem(){
+         System.out.println("Update Item Controller");
+    }
+    public void getItem(){
+
+    }
+
+    public void getAllItems(){
+
+    }
+    public void deleteItem(){
+
+    }
+
+
+}
